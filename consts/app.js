8864c9a65d1f4d8344bcc0f5846d6f28affde793
DonggunLim <Ldonggun6766@gmail.com>
2025-02-12 13:21:46 +0900

    fix: const config 객체로 관리

--- consts/app.js
+++ consts/app.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
-const PORT = 8080;
 
-const FRONT_URL = process.env.FRONT_URL;
-const MONGODB_URL = process.env.MONGODB_URL;
-const JWT_SECRET = process.env.JWT_SECRET;
-
-module.exports = {
-  PORT,
-  FRONT_URL,
-  MONGODB_URL,
-  JWT_SECRET,
+const config = {
+  app: {
+    port: 8080,
+    frontUrl: process.env.FRONT_URL,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    s3: {
+      bucketName: process.env.BUCKET_NAME,
+    },
+  },
 };
+
+module.exports = config;
