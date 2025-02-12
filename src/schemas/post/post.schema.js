2ea85318f61a49d4cad7f8625a4e486d9f48aa77
DonggunLim <Ldonggun6766@gmail.com>
2025-02-12 20:35:06 +0900

    feat: Post schema 추가

---
+++ src/schemas/post/post.schema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { String, ObjectId, Number } = mongoose.Schema.Types;
+
+const postSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: ObjectId,
+      required: true,
+      ref: 'users',
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    comments: {
+      type: [ObjectId],
+      ref: 'comments',
+      default: [],
+    },
+    likes: {
+      type: [ObjectId],
+      ref: 'likes',
+      default: [],
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+  },
+);
+
+const Post = mongoose.model('posts', postSchema);
+module.exports = Post;
