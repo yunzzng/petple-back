9125957f8406b57a6ccf6a0ce8f494a28b84e4ea
DonggunLim <Ldonggun6766@gmail.com>
2025-02-13 15:06:51 +0900

    feat: Post 수정 삭제 라우트 추가

--- src/routes/post/post.router.js
+++ src/routes/post/post.router.js
@@ -2,6 +2,9 @@ const PostController = require('../../controllers/post/post.controller');
 const { token } = require('../../middleware/token.middleware');
 const postsRoutes = require('express').Router();
 
+postsRoutes.get('/:id', PostController.getPost);
 postsRoutes.post('/', token, PostController.addPost);
+postsRoutes.put('/:id', token, PostController.updatePost);
+postsRoutes.delete('/:id', token, PostController.deletePost);
 
 module.exports = postsRoutes;
--- src/service/post/post.service.js
+++ src/service/post/post.service.js
@@ -1,11 +1,36 @@
 const Post = require('../../schemas/post/post.schema');
 
 class PostService {
+  async getPostById(postId) {
+    try {
+      const document = await Post.findById(postId).populate('creator');
+      return document;
+    } catch (error) {
+      throw new Error(`[DB에러] PostService.getPostById`, { cause: error });
+    }
+  }
+
   async createPost(tags, images, description, userId) {
     try {
       await Post.create({ creator: userId, tags, images, description });
     } catch (error) {
       throw new Error(`[DB에러] PostService.createPost`, { cause: error });
+    }
+  }
+
+  async updatePostById(id, post) {
+    try {
+      await Post.findByIdAndUpdate(id, post);
+    } catch (error) {
+      throw new Error(`[DB에러 PostService.updatePostById]`, { cause: error });
+    }
+  }
+
+  async deletePostById(id) {
+    try {
+      await Post.findByIdAndDelete(id);
+    } catch (error) {
+      throw new Error(`[DB에러 PostService.updatePostById]`, { cause: error });
     }
   }
 }
