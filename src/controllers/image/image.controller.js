619ae853d5a808057679abc6fc19f9ae18dbbcf3
DonggunLim <Ldonggun6766@gmail.com>
2025-02-19 19:20:16 +0900

    fix: image route return 형태 통일 및 error 핸들링 변경

--- src/controllers/image/image.controller.js
+++ src/controllers/image/image.controller.js
@@ -1,15 +1,13 @@
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const { PutObjectCommand } = require('@aws-sdk/client-s3');
 const config = require('../../consts/app');
-const AWS_S3 = require('../../config/awsS3.config');
+const awsS3 = require('../../config/awsS3.config');
 const { createError } = require('../../utils/error');
 
 const getPresignedUrl = async (req, res, next) => {
   const { fileName, fileType } = req.query;
   if (!fileName || !fileType) {
-    return res
-      .status(400)
-      .json({ success: false, message: '파일이름과 타입이 필요합니다.' });
+    throw createError(400, '파일이름과 타입이 필요합니다.');
   }
   const params = {
     Bucket: config.aws.s3.bucketName,
@@ -18,15 +16,10 @@ const getPresignedUrl = async (req, res, next) => {
   };
   try {
     const command = new PutObjectCommand(params);
-    const presignedUrl = await getSignedUrl(AWS_S3, command, { expiresIn: 60 });
-    return res.status(200).json({ isError: false, presignedUrl });
+    const presignedUrl = await getSignedUrl(awsS3, command, { expiresIn: 60 });
+    return res.status(200).json({ success: true, presignedUrl });
   } catch (error) {
-    next(
-      createError(
-        500,
-        `presignedUrl 생성하는 중 오류가 발생하였습니다. ${error.message}`,
-      ),
-    );
+    next(error);
   }
 };
 
