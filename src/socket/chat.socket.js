5e93bb935f2a928912ffbda1ae828e1743b2a016
DonggunLim <Ldonggun6766@gmail.com>
2025-02-25 14:19:02 +0900

    feat: Room 입장시 이전 messages 전송 로직 추가

--- src/socket/chat.socket.js
+++ src/socket/chat.socket.js
@@ -1,16 +1,18 @@
-const chatService = require('../service/chat/chat.service');
+const ChatService = require('../service/chat/chat.service');
 
 const ChatNamespace = (io) => {
   const chatNamespace = io.of('/chat');
 
   chatNamespace.on('connection', (socket) => {
-    socket.on('join_room', (roomId) => {
+    socket.on('join_room', async (roomId) => {
       socket.join(roomId);
+      const chat = await ChatService.findChatByRoomId(roomId);
+      socket.emit('prev_message', chat);
     });
 
     socket.on('send_message', async ({ roomId, text, from, to }) => {
       try {
-        await chatService.addMessageToChat({
+        await ChatService.addMessageToChat({
           roomId,
           text,
           from: from.id,
