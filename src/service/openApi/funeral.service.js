efdf3eb48ee22b5de2f1770ad271fc5992a9a282
yunzzng <leejiyun0114@naver.com>
2025-03-04 13:35:40 +0900

    퓨너럴 서비스 수정

--- src/service/openApi/funeral.service.js
+++ src/service/openApi/funeral.service.js
@@ -2,28 +2,29 @@ const FuneralModel = require('../../schemas/openApi/seoul.schema');
 const { formatSeoulFuneralData } = require('../../utils/openApi/fetchFuneralData');
 const { createError } = require("../../utils/error");
 
-const getSeoulFuneralData = async () => {
-  try {
-    const seoulData = await FuneralModel.find({}).lean();
-    if (!seoulData || seoulData.length === 0) {
-      throw createError(404, '서울 장례 데이터가 없습니다.');
+class FuneralService {
+  async getSeoulFuneralData() {
+    try {
+      const seoulData = await FuneralModel.find({}).lean();
+      if (!seoulData || seoulData.length === 0) {
+        throw createError(404, '서울 장례 데이터가 없습니다.');
+      }
+
+      const formattedData = await Promise.all(
+        seoulData.map(async (item) => {
+          try {
+            return await formatSeoulFuneralData(item);
+          } catch (error) {
+            return null;
+          }
+        })
+      );
+
+      return formattedData.filter((data) => data !== null);
+    } catch (error) {
+      throw createError(500, `서울 장례 데이터를 가져오는 중 오류 발생: ${error.message}`);
     }
-
-    const formattedData = await Promise.all(
-      seoulData.map(async (item) => {
-        try {
-          return await formatSeoulFuneralData(item);
-        } catch (error) {
-          return null;
-        }
-      })
-    );
-
-    return formattedData.filter((data) => data !== null);
-  } catch (error) {
-    throw createError(500, `서울 장례 데이터를 가져오는 중 오류 발생: ${error.message}`);
   }
+}
 
-};
-
-module.exports = { getSeoulFuneralData };
+module.exports = new FuneralService();
