5f9ea3643d8895bbffc0d9a47f8823c9400f0a43
yunzzng <leejiyun0114@naver.com>
2025-03-25 17:27:28 +0900

    add cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://petple-front-vert.vercel.app/',
+    origin: 'https://petple-front-vert.vercel.app',
     credentials: true,
   }),
 );
